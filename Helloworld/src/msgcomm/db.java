e3ffc4ad4a017bf136a3d94ea3807a767d552bfd
zhaosenhua <zhaosenhua@shencloudtech.com>
2017-09-22 11:05:57 +0800

    1.update amq  2.add udp multicast 3.mongodb

--- Helloworld/src/msgcomm/db.java
+++ Helloworld/src/msgcomm/db.java
@@ -1,5 +1,17 @@
 package msgcomm;
-
+import  com.mongodb.MongoClient;
+import  com.mongodb.client.MongoDatabase;
 public class db {
-
+	 private MongoClient  mongoClient;
+	 private MongoDatabase mongoDatabase;
+	 public void db() {
+		 try{
+			 //connect mongodb server
+			 mongoClient = new MongoClient("localhost", 27017);
+			 //connect mongodb database
+			 mongoDatabase = mongoClient.getDatabase("test");
+		 }catch(Exception e){
+			 System.err.println(e.getClass().getName() + ":" + e.getMessage());
+		 }
+	 }
 }
